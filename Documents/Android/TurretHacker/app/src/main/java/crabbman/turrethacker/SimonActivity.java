913ba3590e24ba74a8a7b6386c6ce98c97b39580
Davis <crabbydavis@yahoo.com>
2017-03-28 11:26:56 -0600

    Trying to push the two activities

---
+++ Documents/Android/TurretHacker/app/src/main/java/crabbman/turrethacker/SimonActivity.java
@@ -0,0 +1,17 @@
+package crabbman.turrethacker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by crabbydavis on 3/28/17.
+ */
+
+public class SimonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
